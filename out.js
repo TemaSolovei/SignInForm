d8d6e9a731269cde9f75f321da1744b6e5dd6287
Artem Solovev <temasolovei@gmail.com>
2017-11-08 01:35:35 +0500

    Add files via upload

---
+++ out.js
@@ -0,0 +1,4 @@
+function outEP() {
+                        var strEmail = document.getElementsByName("email")[0].value;
+                        var pass = document.getElementsByName("password")[0].value;
+                        alert(strEmail + ":" + pass) }
